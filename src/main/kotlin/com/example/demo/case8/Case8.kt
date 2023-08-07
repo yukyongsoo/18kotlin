79c5d283ae887fabbd2c98cdb2c5ae5a2037672f
yukyongsoo <yozx234@gmail.com>
2023-08-07 14:49:17 +0900

    wip

--- src/main/kotlin/com/example/demo/case8/Case8.kt
+++ src/main/kotlin/com/example/demo/case8/Case8.kt
@@ -1,5 +1,10 @@
 package com.example.demo.case8
 
 class Case8 {
+    val a = ""
 
+//    fun getA(): String {
+//        println(a)
+//        return a
+//    }
 }
--- src/test/kotlin/com/example/demo/Case8Test.kt
+++ src/test/kotlin/com/example/demo/Case8Test.kt
@@ -1,3 +1,13 @@
 package com.example.demo
 
-class Case8Test {}
+import com.example.demo.case8.Case8
+import org.junit.jupiter.api.Test
+
+class Case8Test {
+    @Test
+    fun a() {
+        val case8 = Case8()
+
+        //case8.getA()
+    }
+}
